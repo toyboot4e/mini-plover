d88b67aadb52c90682b331c2e6983649032d0e75
toyboot4e <toyboot4e@gmail.com>
2026-05-17 01:10:46 +0900

    wip

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -2,30 +2,63 @@
 //!
 //! - No `{plover:deleted}` support.
 
+use rustc_hash::FxHashMap;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Output {
+    String(String),
+    // Command
+}
+
 /// Maps sequences to translations and tracks the length of the longest key.
 #[derive(Debug, Clone)]
 pub struct StenoDictionary {
-    dict: (),
-    rev: (),
+    entries: FxHashMap<Vec<String>, Output>,
+    rev: FxHashMap<Output, Vec<Vec<String>>>,
+}
+
+impl StenoDictionary {
+    pub fn new(entries: FxHashMap<Vec<String>, Output>) -> Self {
+        let mut rev = FxHashMap::<Output, Vec<Vec<String>>>::default();
+        for (outline, output) in &entries {
+            if let Some(v) = rev.get_mut(output) {
+                v.push(outline.clone());
+            } else {
+                rev.insert(output.clone(), vec![outline.clone()]);
+            }
+        }
+        Self { entries, rev }
+    }
+
+    /// Looks up an output for the outline.
+    pub fn get(&self, outline: &[String]) -> Option<&Output> {
+        self.entries.get(outline)
+    }
+
+    /// Looks up outlines for the output.
+    pub fn rev_get(&self, output: &Output) -> Option<&Vec<Vec<String>>> {
+        self.rev.get(output)
+    }
 }
 
 #[derive(Debug, Clone)]
-pub struct StenoDictionaryCollection {
+pub struct StenoDictionaryStack {
     //
     dicts: Vec<StenoDictionary>,
 }
 
-impl StenoDictionaryCollection {
+impl StenoDictionaryStack {
     pub fn new(dicts: Vec<StenoDictionary>) -> Self {
         Self { dicts }
     }
 
-    // key??
-    pub fn lookup(&self, key: &str) -> Option<&str> {
-        None
+    /// Looks up an output for the outline.
+    pub fn get(&self, outline: &[String]) -> Option<&Output> {
+        self.dicts.iter().find_map(|d| d.get(outline))
     }
 
-    pub fn reverse_lookup(&self, translation: &str) -> Option<&str> {
-        None
+    /// Looks up outlines for the output.
+    pub fn rev_get(&self, output: &Output) -> Option<&Vec<Vec<String>>> {
+        self.dicts.iter().find_map(|d| d.rev_get(output))
     }
 }
